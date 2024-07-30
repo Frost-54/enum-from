ffa6a4c4941344f83d7f1860bf9c3aa9243950af
~ <91168148+Frost-54@users.noreply.github.com>
2024-07-30 23:05:10 +1000

    Allow mapping to enum with more variants

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 #![allow(non_snake_case)]
 
 use proc_macro::TokenStream;
-use proc_macro2::{TokenStream as TokenStream2, TokenTree as TokenTree2};
-use syn::{parse_macro_input, punctuated::Punctuated, Attribute, Token, ItemEnum, Meta, Path};
-use quote::{quote, ToTokens};
+use proc_macro2::TokenStream as TokenStream2;
+use syn::{parse_macro_input, punctuated::Punctuated, Attribute, ItemEnum, Path, Token, Ident};
+use quote::quote;
 
 const MAPPING_ATTR: &'static str = "mapping";
 
@@ -13,32 +13,59 @@ fn attributeIs(attr: &Attribute, name: &str) -> bool {
              .unwrap_or(false)
 }
 
-fn generateMatchBody(enumDefinition: ItemEnum, fromEnumName: &TokenTree2) -> TokenStream2 {
+fn getMapFromVariant(attr: &Attribute) -> Option<Path> {
+      attr.parse_args_with(Punctuated::<Path, Token![,]>::parse_terminated)
+            .map(|args| {
+                  args.get(0).cloned()
+            }).ok().flatten()
+}
+
+fn getEnumNameFromPath(path: &Path) -> Option<&Ident> {
+       path.segments.get(path.segments.len() - 2).map(|fragment| &fragment.ident)
+}
+
+fn generateMatchBody(enumDefinition: ItemEnum, fromEnumName: &Ident) -> TokenStream2 {
       let toEnumName = &enumDefinition.ident;
+      // For a enum without #[mapping] annotation, generate a match with From::Variant => To::Variant
+      // For a enum with #[mapping] annotations, generate a match with From::$mapping => To::Variant
+      // ONLY for variants with #[mapping] annotations
+      let variantsWithMappingAttr: Vec<_> = enumDefinition.variants.iter().filter_map(|variant| {
+            variant.attrs.iter().find_map(|attr| {
+                  if attributeIs(attr, MAPPING_ATTR) {
+                        let variantName = &variant.ident;
 
-      enumDefinition.variants.iter().fold(quote! {}, |acc, variant| {
-            let variantName = &variant.ident;
-
-            let from = variant.attrs.iter().filter_map(|attr| {
-                  match &attr.meta {
-                        Meta::List(ref list) if attributeIs(attr, MAPPING_ATTR) => {
-                              list.parse_args_with(Punctuated::<Path, Token![,]>::parse_terminated)
-                                    .map(|args| {
-                                          if args.len() != 1 {
-                                                panic!("Expect exactly one enum variant in {} to map from", fromEnumName.to_string());
-                                          }
-                                          args[0].clone().into_token_stream()
-                                    }).ok()
-                        },
-                        _ => None
+                        match getMapFromVariant(attr) {
+                              Some(path) => (getEnumNameFromPath(&path) == Some(fromEnumName))
+                                    .then_some(quote! { #path => #toEnumName::#variantName }),
+                              None => Some(quote! {
+                                    #fromEnumName::#variantName => #toEnumName::#variantName
+                              })
+                        }
                   }
-            }).next().unwrap_or_else(|| quote! { #fromEnumName::#variantName });
+                  else {
+                        None
+                  }
+            })
+      }).collect();
 
-            quote! {
-                  #acc
-                  #from => #toEnumName::#variantName,
-            }
-      })
+      if variantsWithMappingAttr.len() != 0 {
+           variantsWithMappingAttr.iter().fold(quote! {}, |acc, tree| {
+                 quote! {
+                       #acc
+                       #tree,
+                 }
+           })
+      }
+      else {
+            enumDefinition.variants.iter().fold(quote! {}, |acc, variant| {
+                  let variantName = &variant.ident;
+
+                  quote! {
+                        #acc
+                        #fromEnumName::#variantName => #toEnumName::#variantName,
+                  }
+            })
+      }
 }
 
 fn removeAttributes(enumDefinition: &ItemEnum) -> TokenStream2 {
@@ -71,9 +98,7 @@ fn removeAttributes(enumDefinition: &ItemEnum) -> TokenStream2 {
 
 #[proc_macro_attribute]
 pub fn from(attrs: TokenStream, body: TokenStream) -> TokenStream {
-      let fromEnum = TokenStream2::from(attrs).into_iter()
-            .next()
-            .expect("Expect target enum");
+      let fromEnum = parse_macro_input!(attrs as Ident);
       let bodyCopy = body.clone();
       let toEnum = parse_macro_input!(bodyCopy as ItemEnum);
       let toEnumName = toEnum.ident.clone();
@@ -84,7 +109,7 @@ pub fn from(attrs: TokenStream, body: TokenStream) -> TokenStream {
       quote! {
             #enumDefinition
 
-            impl From<#fromEnum> for #toEnumName {
+            impl ::core::convert::From<#fromEnum> for #toEnumName {
                   fn from(other: #fromEnum) -> Self {
                         match other {
                               #matchBody
@@ -94,7 +119,3 @@ pub fn from(attrs: TokenStream, body: TokenStream) -> TokenStream {
       }.into()
 }
 
-#[proc_macro_attribute]
-pub fn mapping(_attrs: TokenStream, tokens: TokenStream) -> TokenStream {
-      tokens
-}
