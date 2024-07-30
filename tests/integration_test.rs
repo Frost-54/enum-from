eb60d463761d6c1aedffb6cdb9d2f82c85095aeb
~ <91168148+Frost-54@users.noreply.github.com>
2024-07-30 23:05:10 +1000

    Allow mapping to enum with more variants test

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -23,6 +23,28 @@ enum TransformCustom {
       V2
 }
 
+#[from(A)]
+#[derive(Debug, PartialEq, Eq)]
+enum TransformExtraSameName {
+      #[mapping]
+      Foo,
+      #[mapping]
+      Bar,
+      #[allow(unused)]
+      Baz
+}
+
+#[from(A)]
+#[derive(Debug, PartialEq, Eq)]
+enum TransformExtraDifferentName {
+      #[mapping(A::Foo)]
+      V1,
+      #[mapping(A::Bar)]
+      V2,
+      #[allow(unused)]
+      V3
+}
+
 #[test]
 fn testTransformSameName() {
       assert_eq!(TransformBySameName::from(A::Foo), TransformBySameName::Foo);
@@ -34,3 +56,16 @@ fn testTransformDifferentName() {
       assert_eq!(TransformCustom::from(A::Foo), TransformCustom::V1);
       assert_eq!(TransformCustom::from(A::Bar), TransformCustom::V2);
 }
+
+#[test]
+fn testTransformExtraSameName() {
+      assert_eq!(TransformExtraSameName::from(A::Foo), TransformExtraSameName::Foo);
+      assert_eq!(TransformExtraSameName::from(A::Bar), TransformExtraSameName::Bar);
+}
+
+#[test]
+fn testTransformExtraDifferentName() {
+      assert_eq!(TransformExtraDifferentName::from(A::Foo), TransformExtraDifferentName::V1);
+      assert_eq!(TransformExtraDifferentName::from(A::Bar), TransformExtraDifferentName::V2);
+}
+
